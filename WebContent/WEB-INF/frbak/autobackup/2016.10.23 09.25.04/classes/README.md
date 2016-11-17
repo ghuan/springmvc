7a45dd7b3497e31198a2a5dbcd90b0625a585493
huan.gao <2223486623@qq.com>
2016-11-17 10:34:31 +0800

    add finereport

--- WebContent/WEB-INF/frbak/autobackup/2016.10.23 09.25.04/classes/README.md
+++
@@ -1,5 +0,0 @@
-# spring3+springmvc3+hibernate4+extjs4.2
-my first project about springmvc</br>
-database : oracle10</br>
-dumpfile import file : ../webContent/database/FN_20160508175815.DMP
-![image](https://github.com/ghuan/springmvc/blob/master/WebContent/images/system.png)
--- WebContent/WEB-INF/frbak/autobackup/2016.10.23 09.25.04/classes/WebContent/subsystem/gsgl/personnel/uploadimage/model/Image.js
+++
@@ -1,9 +0,0 @@
-Ext.define("subsystem.gsgl.personnel.uploadimage.model.Image", {
-			extend : "Ext.data.Model",
-			fields : 
-					[
-		           {name:'id',type:'string'},
-		           {name:'title',type:'string'},
-		           {name:'image',type:'string'},
-		           {name:'updateTime',type:'string'}]
-		});
